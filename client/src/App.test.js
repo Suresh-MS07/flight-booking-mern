a39a16ee4b693a536ac6c7c75b04ad7752f7af9e
SURESH MEWADA <sureshmewada990@gmail.com>
2026-08-24 20:53:09 +0530

    test: isolate router in client smoke test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('react-router-dom', () => {
+  const React = require('react');
+  const passThrough = ({ children }) => React.createElement(React.Fragment, null, children);
+
+  return {
+    BrowserRouter: passThrough,
+    Routes: passThrough,
+    Route: ({ path, element }) => (path === '/' ? element : null),
+    Link: ({ children, to, ...props }) => React.createElement('a', { href: to, ...props }, children),
+    useLocation: () => ({ state: null }),
+    useNavigate: () => jest.fn(),
+    useSearchParams: () => [new URLSearchParams()],
+  };
+}, { virtual: true });
+
 test('renders the SkyBooker landing page', () => {
   render(<App />);
   expect(screen.getByRole('heading', { name: /make your travel dreams reality/i }))
